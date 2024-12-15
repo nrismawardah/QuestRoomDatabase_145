c86b369d5d7b5ab4706b830d7f5d01e6693ec99d
nrismawardah <nrismawardah@gmail.com>
2024-12-15 22:35:33 +0700

    11: Membuat class UpdateMhsViewModel

---
+++ app/src/main/java/com/example/roomdatabase/ui/viewmodel/UpdateMhsViewModel.kt
@@ -0,0 +1,32 @@
+package com.example.roomdatabase.ui.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.roomdatabase.repository.RepositoryMhs
+import com.example.roomdatabase.ui.navigation.DestinasiUpdate
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
+
+class UpdateMhsViewModel (
+    savedStateHandle: SavedStateHandle,
+    private val repositoryMhs: RepositoryMhs
+) : ViewModel() {
+
+    var updateUIState by mutableStateOf(MhsUIState())
+        private set
+
+    private val _nim: String = checkNotNull(savedStateHandle[DestinasiUpdate.NIM])
+
+    init {
+        viewModelScope.launch {
+            updateUIState = repositoryMhs.getMhs(_nim)
+                .filterNotNull()
+                .first()
+                .toUIStateMhs()
+        }
+    }
+}
