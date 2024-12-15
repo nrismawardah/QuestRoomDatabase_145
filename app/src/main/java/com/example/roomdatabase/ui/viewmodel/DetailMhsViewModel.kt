f598a84e42ef026e00b7b9777ae01686756a9c89
nrismawardah <nrismawardah@gmail.com>
2024-12-15 22:22:42 +0700

    9: Menambahkan function Mahasiswa

--- app/src/main/java/com/example/roomdatabase/ui/viewmodel/DetailMhsViewModel.kt
+++ app/src/main/java/com/example/roomdatabase/ui/viewmodel/DetailMhsViewModel.kt
@@ -1,5 +1,14 @@
 package com.example.roomdatabase.ui.viewmodel
 
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import com.example.roomdatabase.data.entity.Mahasiswa
+import com.example.roomdatabase.repository.RepositoryMhs
+import com.example.roomdatabase.ui.navigation.DestinasiDetail
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+
 data class DetailUiState(
     val detailUiState: MahasiswaEvent = MahasiswaEvent(),
     val isLoading: Boolean = false,
@@ -12,4 +21,15 @@ data class DetailUiState(
 
     val isUiEventNotEmpty: Boolean
         get() = detailUiEvent != MahasiswaEvent()
+}
+
+fun Mahasiswa.toDetailUiEvent() : MahasiswaEvent {
+    return MahasiswaEvent(
+        nim = nim,
+        nama = nama,
+        jenisKelamin = jenisKelamin,
+        alamat = alamat,
+        kelas = kelas,
+        angkatan = angkatan
+    )
 }
