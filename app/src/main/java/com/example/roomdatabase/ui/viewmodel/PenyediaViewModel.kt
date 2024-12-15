776ee4210217f28141c86a34dd1f3010f9b958a1
nrismawardah <nrismawardah@gmail.com>
2024-12-15 22:47:36 +0700

    15: Mendaftarkan view model HomeMhsViewModel, DetailMhsViewModel, dan UpdateMhsViewModel

--- app/src/main/java/com/example/roomdatabase/ui/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/roomdatabase/ui/viewmodel/PenyediaViewModel.kt
@@ -2,6 +2,7 @@ package com.example.roomdatabase.ui.viewmodel
 
 import androidx.lifecycle.ViewModelProvider.Factory
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
@@ -9,9 +10,30 @@ import com.example.roomdatabase.KrsApp
 
 object PenyediaViewModel {
     val Factory = viewModelFactory {
+
         initializer {
             MahasiswaViewModel(
                 krsApp().containerApp.repositoryMhs
+            )
+        }
+
+        initializer {
+            HomeMhsViewModel(
+                krsApp().containerApp.repositoryMhs
+            )
+        }
+
+        initializer {
+            DetailMhsViewModel(
+                createSavedStateHandle(),
+                krsApp().containerApp.repositoryMhs,
+            )
+        }
+
+        initializer {
+            UpdateMhsViewModel(
+                createSavedStateHandle(),
+                krsApp().containerApp.repositoryMhs,
             )
         }
     }
