f4f8fdc092f7bacbbcc69ad1b9529e5e3ca6f288
nrismawardah <nrismawardah@gmail.com>
2024-12-15 21:58:18 +0700

    6: Menambahkan function getMhs dan suspend fun deleteMhs dan updateMhs

--- app/src/main/java/com/example/roomdatabase/repository/RepositoryMhs.kt
+++ app/src/main/java/com/example/roomdatabase/repository/RepositoryMhs.kt
@@ -6,4 +6,8 @@ import kotlinx.coroutines.flow.Flow
 interface RepositoryMhs {
     suspend fun insertMhs(mahasiswa: Mahasiswa)
     fun getAllMahasiswa(): Flow<List<Mahasiswa>>
+    fun getMhs(nim: String): Flow<Mahasiswa>
+    suspend fun deleteMhs(mahasiswa: Mahasiswa)
+    suspend fun updateMhs(mahasiswa: Mahasiswa)
+
 }
