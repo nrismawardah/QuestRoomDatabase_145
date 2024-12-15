1a0e196de8a3ad2244e6ffe25147d2bc0bb60034
nrismawardah <nrismawardah@gmail.com>
2024-12-15 22:13:20 +0700

    7: Menambahkan anotasi insert, query, delete, dan update pada MahasiswaDao

--- app/src/main/java/com/example/roomdatabase/data/dao/MahasiswaDao.kt
+++ app/src/main/java/com/example/roomdatabase/data/dao/MahasiswaDao.kt
@@ -1,8 +1,10 @@
 package com.example.roomdatabase.data.dao
 
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
+import androidx.room.Update
 import com.example.roomdatabase.data.entity.Mahasiswa
 import kotlinx.coroutines.flow.Flow
 
@@ -15,4 +17,13 @@ interface MahasiswaDao {
     suspend fun insertMahasiswa(
         mahasiswa: Mahasiswa
     )
+
+    @Query ("SELECT * FROM mahasiswa WHERE nim = :nim")
+    fun getMahasiswa (nim: String) : Flow<Mahasiswa>
+
+    @Delete
+    suspend fun deleteMahasiswa (mahasiswa: Mahasiswa)
+
+    @Update
+    suspend fun updateMahasiswa (mahasiswa: Mahasiswa)
 }
