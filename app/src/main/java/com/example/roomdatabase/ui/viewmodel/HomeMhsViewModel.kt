9a4e1a8eb2a917f41c49db9a5aa3d717623992e6
nrismawardah <nrismawardah@gmail.com>
2024-12-15 21:18:18 +0700

    4: Menambahkan class HomeMhsViewModel yang berisi val HomeUiState

--- app/src/main/java/com/example/roomdatabase/ui/viewmodel/HomeMhsViewModel.kt
+++ app/src/main/java/com/example/roomdatabase/ui/viewmodel/HomeMhsViewModel.kt
@@ -1,6 +1,52 @@
 package com.example.roomdatabase.ui.viewmodel
 
+import androidx.compose.runtime.internal.isLiveLiteralsEnabled
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.roomdatabase.data.entity.Mahasiswa
+import com.example.roomdatabase.repository.RepositoryMhs
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
+
+class HomeMhsViewModel(
+    private val repositoryMhs: RepositoryMhs
+) : ViewModel() {
+
+    val homeUiState: StateFlow<HomeUiState> = repositoryMhs.getAllMahasiswa()
+        .filterNotNull()
+        .map {
+            HomeUiState(
+                listMhs = it.toList(),
+                isLoading = false,
+            )
+        }
+        .onStart {
+            emit(HomeUiState(isLoading = true))
+            delay(900)
+        }
+        .catch {
+            emit(
+                HomeUiState(
+                    isLoading = false,
+                    isError = true,
+                    errorMessage = it.message ?: "Terjadi kesalahan"
+                )
+            )
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = HomeUiState(
+                isLoading = true,
+            )
+        )
+}
 
 data class HomeUiState (
     val listMhs: List<Mahasiswa> = listOf(),
